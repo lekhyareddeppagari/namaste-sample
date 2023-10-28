92bdd2d6170a7d1232ce0583e8ce958f47ad84cc
lekhyareddeppagari <rlekhya.16@gmail.com>
2023-10-28 17:28:27 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading=React.createElement("div",{id:"parent"},
+React.createElement("div",{id:"child"},
+React.createElement("h1",{id:"heading1"},"hello vinay")));
+            
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading);
